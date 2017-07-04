df448dc668e5b19a55a85548800c6463c32efb5b
kassemshehady <kassemshehady@gmail.com>
2017-07-04 16:25:56 +0300

    Adding Android App

--- Web/Extentions/HtmlExtensions.cs
+++ Web/Extentions/HtmlExtensions.cs
@@ -8,8 +8,6 @@ namespace Web.Extentions
 {
     public static class HtmlExtensions
     {
-
-
         //we have two pagers:
         //The first one can have custom routes
         //The second one just adds query string parameter
